7b1be788b8bdbab39038102d8ff5ea29609b31b2
Andres Garcia <andresgarcia@seven4n.com>
2017-09-17 15:17:37 -0500

    se implementa filtro de tabla

--- Lab2-MueblesDeLosAlpes/Lab2-MueblesDeLosAlpes-war/src/java/com/losalpes/beans/ClienteBean.java
+++ Lab2-MueblesDeLosAlpes/Lab2-MueblesDeLosAlpes-war/src/java/com/losalpes/beans/ClienteBean.java
@@ -11,6 +11,9 @@ import com.losalpes.servicios.ServicioClienteImpl;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import org.primefaces.component.datatable.DataTable;
+import org.primefaces.context.RequestContext;
 
 /**
  * Managed bean encargado de CRUD de Clientes
@@ -26,12 +29,32 @@ public class ClienteBean {
     private Cliente cliente;
     
     /**
+     * Lista de Clientes filtrados
+     */
+
+    private List<Cliente> clientesFiltrados;
+    
+    /**
      * IServicioCliente
      */
     private IServicioCliente clienteService;
-    
+    /**
+     * Documento d3el Cliente a Eliminar
+     */
     private String numeroDocumento;
-
+    /**
+     * Documento d3el Cliente a filtrar
+     */
+    private String numeroDocumentoFilter = "";
+    /**
+     * Nombre del Cliente a Filtrar
+     */
+    private String nombreFilter = "";
+    /**
+     * Email del Cliente a Filtrar
+     */
+    private String email = "";
+    
     public ClienteBean() {
         cliente = new Cliente();
         this.clienteService = new ServicioClienteImpl();
@@ -50,17 +73,32 @@ public class ClienteBean {
      */
     public void eliminarCliente()
     {
+    DataTable dataTable = (DataTable) FacesContext.getCurrentInstance().getViewRoot().findComponent("form:dataTable");
         clienteService.eliminarCliente(numeroDocumento);
         numeroDocumento = "";
     }
-    
+
+     /**
+     * Actualiza un Cliente 
+     */
+    public void actualizarCliente()
+    {
+        clienteService.actualizarCliente(cliente);
+        cliente=new Cliente();
+    }
        /**
      * Devuelve una lista con los clientes del sistema Flitrados
      * @return muebles clientes del sistema
      */
+    public List<Cliente> getClientesFilter()
+    {
+        return clienteService.darClientes(numeroDocumentoFilter, nombreFilter, email);
+    }
+    
+    
     public List<Cliente> getClientes()
     {
-        return clienteService.darClientes("", "", "");
+        return clienteService.darClientes(numeroDocumentoFilter, nombreFilter, email);
     }
     
     public Cliente getCliente() {
@@ -79,4 +117,37 @@ public class ClienteBean {
         this.numeroDocumento = numeroDocumento;
     }
 
+    public String getNumeroDocumentoFilter() {
+        return numeroDocumentoFilter;
+    }
+
+    public void setNumeroDocumentoFilter(String numeroDocumentoFilter) {
+        this.numeroDocumentoFilter = numeroDocumentoFilter;
+    }
+
+    public String getNombreFilter() {
+        return nombreFilter;
+    }
+
+    public void setNombreFilter(String nombreFilter) {
+        this.nombreFilter = nombreFilter;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public List<Cliente> getClientesFiltrados() {
+        return clientesFiltrados;
+    }
+
+    public void setClientesFiltrados(List<Cliente> clientesFiltrados) {
+        this.clientesFiltrados = clientesFiltrados;
+    }
+
+    
 }
